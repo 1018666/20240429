5438796ce67b2aa5b7d4d2c384fc4f1d214af17e
1018666 <150640222+1018666@users.noreply.github.com>
2024-04-29 09:45:07 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,5 +7,8 @@ capture.size(320,240);//設定顯示畫面大小
 
 function draw() {
   background(220);
-  image(capture,mouseX, mouseY)
+  push()
+  translate(width/2-160, height/2-120)
+  image(capture,0,0)
+  pop()
 }
